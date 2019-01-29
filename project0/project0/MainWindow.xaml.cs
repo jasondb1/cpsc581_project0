b36f1601e9d5faafaf447c1b5171924783497d3b
Jason <jason@teamdeboer.ca>
2019-01-29 10:48:08 -0700

    initial commit

---
+++ project0/project0/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace project0
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public UserButton userButton = new UserButton();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //event Handlers
+            this.KeyUp += OnMainWindowKeyUp;
+            this.MainButton.Click += OnMainWindowButtonClick;
+
+
+        }
+
+        private void OnMainWindowKeyUp(object sender, KeyEventArgs e)
+        {
+            // if the user 'releases' escape, close the program
+            if (e.Key == Key.Escape)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void OnMainWindowButtonClick(object sender, KeyEventArgs e)
+        {
+            this.userButton.processClick();
+        }
+
+    }
+}
